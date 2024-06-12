36bee202ab2dd8f0863aaacf981abb7dc5f0ced1
egeakman <me@egeakman.dev>
2024-06-13 02:31:22 +0300

    Add fallback keys

--- src/utils/sort.py
+++ src/utils/sort.py
@@ -1,19 +1,27 @@
 class Sort:
     @staticmethod
-    def sort_nested(data):
+    def sort_nested(data, sort_keys=None):
         """
         TODO: This is probably too generic and not the best way, but it works for now
         """
+        if sort_keys is None:
+            sort_keys = ["start", "code", "title", "name"]
+
+        def get_sort_key(item):
+            return tuple(item.get(key, "") for key in sort_keys)
+
         if isinstance(data, dict):
-            return {key: Sort.sort_nested(value) for key, value in sorted(data.items())}
+            return {
+                key: Sort.sort_nested(value, sort_keys)
+                for key, value in sorted(data.items())
+            }
         elif isinstance(data, list):
             if all(isinstance(item, dict) for item in data):
-                # Sort by start time in schedule
                 return sorted(
-                    (Sort.sort_nested(item) for item in data),
-                    key=lambda x: x.get("start", ""),
+                    (Sort.sort_nested(item, sort_keys) for item in data),
+                    key=get_sort_key,
                 )
             else:
-                return sorted(Sort.sort_nested(item) for item in data)
+                return sorted(Sort.sort_nested(item, sort_keys) for item in data)
         else:
             return data
