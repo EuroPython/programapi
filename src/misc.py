802b827221a34ad71e10eff528eb0449cf8cc673
Ege Akman <me@egeakman.dev>
2025-11-10 15:08:28 -0500

    Add schedule for 2025 (#1)
    * Add rooms for 2025
    
    * unexclude schedule

--- src/misc.py
+++ src/misc.py
@@ -36,22 +36,8 @@ class Room(Enum):
     Rooms at the conference venue, this can change year to year
     """
 
-    # Tutorial/workshop rooms
-    club_a = "Club A"
-    club_b = "Club B"
-    club_c = "Club C"
-    club_d = "Club D"
-    club_e = "Club E"
-    club_h = "Club H"
-
-    # Conference rooms
-    forum_hall = "Forum Hall"
-    terrace_2a = "Terrace 2A"
-    terrace_2b = "Terrace 2B"
-    north_hall = "North Hall"
-    south_hall_2a = "South Hall 2A"
-    south_hall_2b = "South Hall 2B"
-    exhibit_hall = "Exhibit Hall"
+    main_stream = "Main Stream"
+    other_stream = "Activities & Open Spaces"
 
 
 class EventType(Enum):
